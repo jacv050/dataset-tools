965be9e89dfee3a83ba6ea9a42ac675413370396
John <jacv050@gmail.com>
2016-12-10 22:53:51 +0100

    Added option to save and apply mask

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@
 #define PARAM_BACKGROUND "background"
 #define PARAM_OBJECT "object"
 #define PARAM_OUTPUTIMAGE "outputimage"
-#define PARAM_APPLYMASK "applymask"
 #define PARAM_GAUSSIAN_ITERATIONS "gaussianiterations"
-#define PARAM_RESIZE "resize"
+#define PARAM_WINDOW_RATIO "windowratio"
 
 const std::string window_name = "SubstractImgGrabcutGUI";
 
@@ -21,13 +20,17 @@ cv::Mat1b imgClose, imgOpen, oriDiff;
 cv::Mat mObject;
 cv::Mat1b mask_fgpf;
 cv::Mat1b grabCutMask;
+std::vector<int> compression_params;
+bool maskApplied;
 
 const int KOPENINGSIZE = 5;
 const int KCLOSINGSIZE = 51;
 
 void Morphology_OpeningClean( int, void* );
 void Morphology_ClosingForm( int, void* );
-void Button_Grabcut(int, void*);
+void ApplyGrabcut();
+void ApplyMask();
+void SaveMaskApplied(const std::string&);
 
 bool parse_command_line_options(boost::program_options::variables_map &variables_map, const int &argc, char **argv){
 
@@ -38,12 +41,11 @@ bool parse_command_line_options(boost::program_options::variables_map &variables
 			(PARAM_HELP, "Produce help message")
 			(PARAM_GAUSSIAN_ITERATIONS, boost::program_options::value<int>()->default_value(5),
 					"How many times apply gaussian filter")
+			(PARAM_WINDOW_RATIO, boost::program_options::value<int>()->default_value(2), "Ratio of window in first charge")
 			(PARAM_BACKGROUND, boost::program_options::value<std::string>()->required(), "Image background")
 			(PARAM_OBJECT, boost::program_options::value<std::string>()->required(), "Object to substract")
-			(PARAM_APPLYMASK, boost::program_options::value<bool>()->default_value(false), "Apply mask to real image")
 			(PARAM_OUTPUTIMAGE, boost::program_options::value<std::string>()->default_value("finalimage.png"),
-					"Name of final image(If NOT apply mask, it will save mask file)")
-			(PARAM_RESIZE, boost::program_options::value<bool>()->default_value(true), "Resize image");
+					"Name of final image(If NOT apply mask, it will save mask file)");
 
 		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), variables_map);
 
@@ -65,11 +67,6 @@ bool parse_command_line_options(boost::program_options::variables_map &variables
 
 
 int main(int argc, char **argv){
-
-	/// Create window
-	cv::namedWindow(window_name, CV_WINDOW_NORMAL | CV_WINDOW_OPENGL);
-
-	std::vector<int> compression_params;
 	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 	compression_params.push_back(9);
 
@@ -79,14 +76,18 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
+	/// Create window
+	cv::namedWindow(window_name, CV_WINDOW_NORMAL | CV_WINDOW_OPENGL);
+
 	//General params
+	std::string sImageCut = "imagecut.jpg";
 	std::string sBackground = variablesMap[PARAM_BACKGROUND].as<std::string>();
 	std::string sObject = variablesMap[PARAM_OBJECT].as<std::string>();
 	std::string sOutputimage = variablesMap[PARAM_OUTPUTIMAGE].as<std::string>();
 	int gaussianIterations = variablesMap[PARAM_GAUSSIAN_ITERATIONS].as<int>();
-	bool bApplyMask = variablesMap[PARAM_APPLYMASK].as<bool>();
-	bool bResize = variablesMap[PARAM_RESIZE].as<bool>();
+	int winRatio = variablesMap[PARAM_WINDOW_RATIO].as<int>();
 
+	maskApplied=false;
 	mObject = cv::imread(sObject);
 	cv::Mat mBackground = cv::imread(sBackground);
 	cv::Size initSize = mObject.size();
@@ -109,19 +110,38 @@ int main(int argc, char **argv){
 	//Convert image to B&W
 	cv::threshold(oriDiff, oriDiff, 25, 255, CV_THRESH_BINARY);
 
+	openingSize = KOPENINGSIZE;
+	closingSize = KCLOSINGSIZE;
+
+	cv::resizeWindow(window_name, mObject.cols/winRatio, mObject.rows/winRatio);
 	//Apply morphology operation. Opening to get object  //Size odd 2n+1
 	cv::createTrackbar("Opening to get object. Size: ", window_name, &openingSize, 10, Morphology_OpeningClean);
 	cv::createTrackbar("Closing to clean image. Size: ", window_name, &closingSize, 100, Morphology_ClosingForm);
-	cv::createButton("Apply GrabCut", Button_Grabcut, NULL, CV_PUSH_BUTTON, 0);
 
 	//Defualt values
+	Morphology_OpeningClean(openingSize,0);
+	Morphology_ClosingForm(closingSize,0);
 
-	openingSize = KOPENINGSIZE;
-	closingSize = KCLOSINGSIZE;
-	Morphology_OpeningClean(0,0);
-	Morphology_ClosingForm(0,0);
-
-	cv::waitKey(0);
+	std::cout << "Press 's' to save the mask of image with the name of '--outputimage'" << std::endl;
+	std::cout << "Press 'g' to apply grabcut" << std::endl;
+	std::cout << "Press 'm' to apply mask" << std::endl;
+	std::cout << "Press 'a' to save image with mask applied" << std::endl;
+	char c;
+	while((c=cv::waitKey(0))!='q'){
+		if(c=='g' || c=='G'){
+			std::cout << "Grabcut applied" << std::endl;
+			ApplyGrabcut();
+		}else if(c=='s' || c=='S'){
+			std::cout << "Mask saved in: " << sOutputimage << std::endl;
+			cv::imwrite(sOutputimage, grabCutMask, compression_params);
+		}else if(c=='m' || c=='M'){
+			std::cout << "Mask applied" << std::endl;
+			ApplyMask();
+		}else if(c=='a' || c=='A'){
+			std::cout << "Save image with mask applied in: " << sImageCut << std::endl;
+			SaveMaskApplied(sImageCut);
+		}
+	}
 
 	return 1;
 }
@@ -141,8 +161,7 @@ void Morphology_ClosingForm( int, void* ){
 	cv::imshow(window_name, imgClose);
 }
 
-void Button_Grabcut(int, void*){
-
+void ApplyGrabcut(){
 	//Mask
 	cv::Mat1b markers(imgClose.rows, imgClose.cols);
 	markers.setTo(cv::GC_PR_FGD);
@@ -170,4 +189,19 @@ void Button_Grabcut(int, void*){
 	cv::Mat1b mBWObject = cv::Mat1b::ones(mObject.rows, mObject.cols) * 255;
 	mBWObject.copyTo(grabCutMask, mask_fgpf);
 	cv::imshow( window_name, grabCutMask);
+}
+
+void ApplyMask(){
+	maskApplied=true;
+	cv::Mat maskedObject;
+	mObject.copyTo(maskedObject, mask_fgpf);
+	cv::imshow(window_name, maskedObject);
+}
+
+void SaveMaskApplied(const std::string& name){
+	cv::Mat maskedObject;
+	if(!maskApplied)
+		ApplyGrabcut();
+	mObject.copyTo(maskedObject, mask_fgpf);
+	cv::imwrite(name, maskedObject, compression_params);
 }
