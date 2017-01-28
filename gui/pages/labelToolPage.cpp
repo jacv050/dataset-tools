c890db9a3061a38ab4c3d745738c9ae1d0d13e19
John <jacv050@gmail.com>
2017-01-28 12:56:55 +0100

    Interface for label tool page

---
+++ gui/pages/labelToolPage.cpp
@@ -0,0 +1,18 @@
+/*
+ * labelToolPage.cpp
+ *
+ *  Created on: 28/01/2017
+ *      Author: john
+ */
+
+#include <gui/pages/labelToolPage.h>
+
+labelToolPage::labelToolPage() {
+	// TODO Auto-generated constructor stub
+
+}
+
+labelToolPage::~labelToolPage() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ gui/pages/labelToolPage.h
@@ -0,0 +1,22 @@
+/*
+ * labelToolPage.h
+ *
+ *  Created on: 28/01/2017
+ *      Author: john
+ */
+
+#ifndef LABELTOOLPAGE_H_
+#define LABELTOOLPAGE_H_
+
+#include "ui_labelToolPage.h"
+
+class labelToolPage {
+public:
+	labelToolPage();
+	virtual ~labelToolPage();
+
+private:
+	Ui::labelToolPage *mUi;
+};
+
+#endif /* LABELTOOLPAGE_H_ */
