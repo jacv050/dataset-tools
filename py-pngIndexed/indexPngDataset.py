fe5f3174ce773d49af824bcc041a9c3773390b04
John <jacv050@gmail.com>
2017-02-15 14:45:55 +0100

    Fixed route python

--- py-pngIndexed/indexPngDataset.py
+++ py-pngIndexed/indexPngDataset.py
@@ -8,7 +8,7 @@ import csv
 
 def processDataset(dataset):
     dataset_dict = {}
-    with open(dataset,'rb') as csvfile:
+    with open(dataset,'r') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in spamreader:
             colour = row[2].split(',')
@@ -20,8 +20,8 @@ def processDataset(dataset):
 def main(argv):
     arguments = len(argv)
     if arguments <= 3:
-        print "You must introduce:"
-        print "\tscript.py dataset.csv output.png list_of_ masks_routes(python-script dataset mask1[:object_name] mask2[:object_name] mask3[:object_name]...)"
+        print("You must introduce:")
+        print("\tscript.py dataset.csv output.png list_of_ masks_routes(python-script dataset mask1[:object_name] mask2[:object_name] mask3[:object_name]...)")
         sys.exit(0)    
 
     dataset = processDataset(argv[1])
@@ -34,7 +34,7 @@ def main(argv):
     max_colour = np.iinfo('uint8').max
     map_palette = [0]*(max_colour+1)*3
     indexed = []
-    pos_delimiter = str(argv[3]).find(':')
+    pos_delimiter = str(argv[3]).find(';')
     if(pos_delimiter == -1):
         indexed = np.array(Image.open(str(argv[3])).convert('P'))*0
     else:
@@ -42,27 +42,27 @@ def main(argv):
 
     #Indexing mas in png
     for i in range(3, arguments):
-        print "Processing", argv[i]
+        print("Processing " + argv[i] + "\n")
         splitted = argv[i].split('/')
         name = splitted[len(splitted)-1]
         key = ""
         nameFile = ""
-        pos_delimiter = name.find(':')
+        pos_delimiter = name.find(';')
         if(pos_delimiter == -1):
             key = name.split('.')[0]
             nameFile = argv[i]
         else:
             key = name[pos_delimiter+1:len(name)]
-            nameFile = argv[i].split(':')[0]
+            nameFile = argv[i].split(';')[0]
             #print key
 
-        print key
+        print(key)
         data = dataset[key]
         im = Image.open(nameFile).convert('P')
         mask = np.array(im)
         indexed[mask > max_colour/2] = data[0]
         map_palette[data[0]*3 : (data[0]+1)*3] = data[1]
-
+        
     imfile = Image.fromarray(indexed)
     imfile.putpalette(map_palette)
     imfile.save(argv[2])
